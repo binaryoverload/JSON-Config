d8c0f8299f34d18e6aaca3244f200b50c29ce5e3
William Oldham <wegg7250@gmail.com>
2017-06-23 11:29:15 +0100

    MORE TESTING

--- src/test/java/io/github/binaryoverload/JSONConfigTest.java
+++ src/test/java/io/github/binaryoverload/JSONConfigTest.java
@@ -1,5 +1,6 @@
 package io.github.binaryoverload;
 
+import com.google.gson.JsonObject;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -13,6 +14,13 @@ public class JSONConfigTest {
     public void testSet() {
         JSONConfig config = new JSONConfig(this.getClass().getClassLoader().getResourceAsStream("test.json"));
         config.set("glossary.title", "Hi there");
-        assertTrue(((String) config.getObject("glossary.title")).equalsIgnoreCase("Hi there"));
+        assertTrue((config.getElement("glossary.title").getAsString()).equalsIgnoreCase("Hi there"));
     }
+
+    @Test
+    public void testGet() {
+        JSONConfig config = new JSONConfig(this.getClass().getClassLoader().getResourceAsStream("test.json"));
+        assertTrue((config.getElement("glossary.GlossDiv.title").getAsString()).equalsIgnoreCase("S"));
+    }
+
 }
